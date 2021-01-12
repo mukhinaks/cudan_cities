564f07d3b1dbfcdee2933841f53833f052a2a81d
Ksenia Mukhina <mukhinaks@users.noreply.github.com>
2021-01-12 14:02:31 +0200

    upd for museums

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,22 +9,23 @@ import pandas as pd
 import pydeck as pdk
 import datetime
 from dateutil.relativedelta import relativedelta
+import json
 
 folder = 'data/'   
-cities = {'London, UK': 'london', 
-      'Saint Petersburg, Russia': 'spb',
-      'Vienna, Austria': 'wienna', 
-      'New York City, USA': 'new-york', } #'tokyo'
-
-coordinates = {'london':(51.507222, -0.1275), 
-          'spb':(59.9375, 30.308611),
-          'wienna':(48.2, 16.366667), 
-          'new-york':(40.71274, -74.005974), }
-
-def hex_to_rgb(color_string):
-    h = color_string.lstrip('#')
-    rgb_color = [int(h[i:i+2], 16) for i in (0, 2, 4)]
-    return rgb_color
+museums = {
+    'Hermitage Museum, Saint Petersburg, Russia':0,
+    'Museum of Modern Art (MoMA), New York, USA':0,
+    'Louvre, Paris, France':0,
+    'British Museum, London, UK':0,
+    'Museo Nacional del Prado, Spain':0,
+    'Louvre-Lens, Lille, France':0,
+    'Louvre Abu Dhabi, United Arab Emirates':0,
+    'Museum of Contemporary Art, Los Angeles, USA':0,
+    'National Palace Museum, Taipei':0,
+    'Dallas Museum of Art, Dallas, USA':0,
+    'Vatican Museums, Vatican':0,
+    'Garage Museum of Contemporary Art, Moscow, Russia':0,
+ } 
 
 def date_range():
     start = datetime.datetime(2017, 1, 1)
@@ -44,89 +45,25 @@ def load_data(city):
     
     return map_data
 
-
 def main():
     main_area = st.empty()    
     main_window(main_area)
     
-    st.sidebar.title('Hi')
-    page = st.sidebar.selectbox("See pages",
-        ["Genral information", "City"])
-    
-    if page == "City":
-        main_area.empty()
-        complex_data()
-    
 def main_window(area):
-    stats = []
-    for city, fname in cities.items():
-        map_data = pd.read_csv(folder + fname + '-filtered.csv')
-        c = {}
-        c['City'] = city
-        c['Instagram locations'] = len(map_data)
-        c['Posts since 2017'] = sum(map_data.iloc[:, 6:].sum(axis=1))
-        stats.append(c)
+    with open(folder + 'museums-wiki.json', 'r') as f:
+        museum_info = json.load(f) 
         
-    df = pd.DataFrame(stats)
+    st.title('Museums')
     
-    with area.beta_container():
-        st.title('City analysis')
-        
-        st.subheader('City coverage by Instagram locations')
-        
-        #st.write("Here's our first attempt at using data to create a table:")
-        st.write(df)
-        
-        option = st.selectbox(
-            'Select city',
-             list(cities.keys()))
-        
-        st.map(load_data(cities[option]))
-
-def filter_data(map_data, date, value = 0):
-    tmp = map_data[map_data[date] > value]
-    return tmp[['lon','lat', date]]
+    option = st.selectbox(
+            'Select museum',
+             list(museums.keys()))
     
-def complex_data():     
-     option = st.selectbox(
-            'Select city',
-             list(cities.keys()), key=1)
-     
-     map_data = load_data(cities[option])
-     
-     sl_date = st.select_slider('', date_range())
 
-     date = str(sl_date.year) + '-' + str(sl_date.month)
-     
-     st.pydeck_chart(pdk.Deck(
-         map_style='mapbox://styles/mapbox/light-v9',
-         initial_view_state=pdk.ViewState(
-             latitude=coordinates[cities[option]][0],
-             longitude=coordinates[cities[option]][1],
-             zoom=10,
-             pitch=0,
-         ),
-         layers=[
-             pdk.Layer(
-                'HexagonLayer',
-                data= filter_data(map_data, date, 5),
-               get_position='[lon, lat]',
-                radius=200,
-                elevation_scale=0,
-                pickable=True,
-                extruded=False,
-                color_aggregation="SUM",
-                get_color_weight=date,
-                color_range=[[241,238,246],[212,185,218],[201,148,199],
-                             [223,101,176],[221,28,119],[152,0,67]]
-
-             ),
+    col1, col2 = st.beta_columns(2)
     
-         ],
-     ))
-                    #    get_fill_color=[255,0,0],
-                #"[255, (1 -" + date + "/" + str(map_data[date].max()) + ") * 255, 0]",
-              #  get_line_color=[255, 255, 255],
-             #   line_width_min_pixels=2,
+    col1.write(museum_info[option]['summary'])
+    col2.image(museum_info[option]['img_link'])
+
 if __name__ == "__main__":
     main()
